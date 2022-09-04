279df8a573e1d20a1fa3505cfc8eb88b2651df8d
Frédéric Jacques <gary.bob88@gmail.com>
2022-09-04 10:53:47 -0400

    try fix pulumi_eks install error

--- setup.sh
+++ setup.sh
@@ -2,17 +2,25 @@
 
 # exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
 set -e -x
+
 # Download and install required tools.
 # pulumi
 curl -fsSL https://get.pulumi.com/ | bash
 export PATH=$PATH:$HOME/.pulumi/bin
+
 # Login into pulumi. This will require the PULUMI_ACCESS_TOKEN environment variable
 pulumi login
+
 # update the GitLab Runner's packages
 apt-get update -y
 apt-get install sudo -y
+
+apk update
+apk add make automake gcc g++ subversion python3-dev
+
 # nodejs
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
+
 # install python dependencies
 apt-get install python3-venv -y
